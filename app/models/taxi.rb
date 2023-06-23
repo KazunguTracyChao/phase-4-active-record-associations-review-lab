e748b7cfd1e0410c7e281e724c57c4cb8d38986e
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-06-23 14:35:49 +0300

    Completed assignment

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -2,38 +2,6 @@ class Taxi < ApplicationRecord
     has_many :rides
     has_many :passengers, through: :rides
   end
-  Passenger has many rides:
-  The error message "expected: #<Passenger id: nil, passenger_name: nil, created_at: nil, updated_at: nil> got: nil" suggests that the passenger attribute is not being set correctly in the Ride model. Double-check if you have set up the association properly:
-  ruby
-  Copy code
-  class Ride < ApplicationRecord
-    belongs_to :passenger
-    belongs_to :taxi
-  end
-  Ride belongs to a passenger:
-  The error message "unknown attribute 'passenger_id' for Ride" suggests that the passenger_id attribute is still missing in the rides table. Please make sure you ran the migration to add the passenger_id foreign key correctly.
-  
-  Ride belongs to a taxi:
-  The error message "unknown attribute 'passenger_id' for Ride" suggests that you are still using the incorrect attribute name (passenger_id) for the association. Ensure that you are using taxi_id instead when creating a new ride.
-  
-  Taxi has many passengers through rides:
-  The error message "can't write unknown attribute passenger_id" suggests that the passenger_id attribute is still missing in the rides table. Please check if you ran the migration to add the passenger_id foreign key correctly. Also, make sure you have defined the associations correctly in the models.
-  
-  Taxi has many rides:
-  The error message "expected: #<Taxi id: nil, driver_name: nil, car_plate: nil, made: nil, created_at: nil, updated_at: nil> got: nil" suggests that the taxi attribute is not being set correctly in the Ride model. Double-check if you have set up the association properly:
-  
-  ruby
-  Copy code
-  class Ride < ApplicationRecord
-    belongs_to :passenger
-    belongs_to :taxi
-  end
-  After making these changes, run the RSpec tests again to verify if the issues have been resolved:
-  
-  bash
-  Copy code
-  bundle exec rspec
-  Make sure all the tests pass without any failures. If you continue to experience issues, please provide the code for your models and migrations, and I'll be happy to help you further.
   
   
   
